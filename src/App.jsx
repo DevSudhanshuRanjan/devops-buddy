96f9e9685e3b46ae2f6f700314ac06c44cd59d64
vaishnavi joshi <vaishnavi.16947@stu.upes.ac.in>
2026-03-31 00:11:08 +0530

    Backup before changing app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-useEffect(() => {
-  fetch("http://localhost:5000/api/message")
-    .then(res => res.json())
-    .then(data => console.log(data.message));
-}, []);
+import { useEffect } from "react";
+
+function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/message")
+      .then(res => res.json())
+      .then(data => console.log(data.message));
+  }, []);
+
+  return <h1>DevOps Buddy 🚀</h1>;
+}
+
+export default App;
